2f2364ce05962e0b3fcb578d535c48c6683414cc
Shemant <kumarshemant1@gmail.com>
2025-06-21 22:55:32 +0530

    corrections

--- src/test/java/app/parking/ParkingManagementApplicationTests.java
+++ src/test/java/app/parking/ParkingManagementApplicationTests.java
@@ -1,13 +1,13 @@
-package app.parking;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ParkingManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package app.parking;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ParkingManagementApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
