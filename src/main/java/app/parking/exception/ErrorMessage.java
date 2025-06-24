e3c99d5c1937cc9c4cf9d89bec13998f04c9699b
Shemant <kumarshemant1@gmail.com>
2025-06-24 20:21:02 +0530

    added global exception handler

--- src/main/java/app/parking/exception/ErrorMessage.java
+++ src/main/java/app/parking/exception/ErrorMessage.java
@@ -1,5 +1,18 @@
 package app.parking.exception;
 
-public class ErrorMessage {
+import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+	
+	private LocalDateTime dateTime;
+	private String message;
 }
